f54b819a0de0d83cd5dc63a6f047fc50b37676ee
ito-lvgs <yuichi.ito@leverages.jp>
2020-02-05 15:45:15 +0900

    add: pingでtimeoutチェック

--- db.go
+++ db.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
+	"time"
 )
 
 type JOB struct {
@@ -17,23 +19,28 @@ type JOB struct {
 }
 
 func db(dbUser string, dbPass string, hostName string) {
-	dns := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=center sslmode=disable", hostName, dbUser, dbPass)
+	dns := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=center sslmode=disable connect_timeout=5", hostName, dbUser, dbPass)
 	db, err := sql.Open("postgres", dns)
 	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*8)
+	defer cancel()
+
+	log.Println("result: ===========")
 
 	if err != nil {
 		panic(err)
 	}
 
-	rows, err := db.Query("select text from writing_job limit 3")
-	var jobs []JOB
-	for rows.Next() {
-		var job JOB
-		rows.Scan(&job.Text)
-		jobs = append(jobs, job)
+	var text string
+	row := db.QueryRowContext(ctx, "select text from writing_job limit 1")
+	err = row.Scan(&text)
+	log.Println(text)
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Println("==== timeout expired ====")
+		log.Println(err)
 	}
 
-	for _, b := range jobs {
-		log.Println(b.Text)
-	}
 }
